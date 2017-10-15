252d9b1da1da48c6036792e962f1edcd8f68623d
Roman Baygildin <rbaygildin95@gmail.com>
2017-10-15 15:53:47 +0300

    Appointments

--- apps/appointments/urls.py
+++ apps/appointments/urls.py
@@ -4,5 +4,7 @@ from apps.appointments.views import *
 
 urlpatterns = [
     url(r'^create$', create_action, name='create'),
-    url(r'^show/(?P<appointment_id>\d+)$', show_action, name='show')
+    url(r'^show/(?P<appointment_id>\d+)$', show_action, name='show'),
+    url(r'^finish/(?P<appointment_id>\d+)$', finish_action, name='finish'),
+    url(r'^cancel/(?P<appointment_id>\d+)$', cancel_action, name='cancel'),
 ]
