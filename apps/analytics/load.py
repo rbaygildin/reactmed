db9175a68e0a49165b849a7d162ceb5f5aa496f1
Roman Baygildin <rbaygildin95@gmail.com>
2017-10-15 04:01:35 +0300

    Visualization

--- apps/analytics/load.py
+++ apps/analytics/load.py
@@ -1,7 +1,7 @@
 import numpy as np
-from pandas import pivot_table, DataFrame
+from pandas import DataFrame
 
-from apps.analytics.etl import PostgresDataLoader
+from apps.core.models import TestRec
 
 
 def scale(series):
@@ -16,48 +16,41 @@ def standardize(series):
     return (series - mean) / std
 
 
+def rec_to_row(class_col=None, feature_cols=None):
+    def mapper(rec):
+        row = {}
+        for ind_id, ind in rec['real_inds'].items():
+            if ind_id in feature_cols:
+                continue
+            row[rec['real_inds'][ind_id]['name']] = ind['value']
+        row['patient_id'] = rec['patient_id']
+        if class_col is not None:
+            row[rec['text_inds'][class_col]['name']] = rec['text_inds'][class_col]['value']
+        return row
+
+    return mapper
+
+
 def data_load(**kwargs):
-    load_pattern = kwargs.get('load_pattern', 'fc')
     test = kwargs.get('test')
-    feature_cols = kwargs.get('feature_cols')
-    class_col = kwargs.get('class_col')
+    feature_cols = kwargs.get('feature_cols', None)
+    class_col = kwargs.get('class_col', None)
     normalize = kwargs.get("normalize", None)
-    if load_pattern == 'fc':
-        df = PostgresDataLoader.load_data(test=test, features=feature_cols, classes=class_col, load_pattern='fc')
-        if df is None or len(df) == 0:
-            raise ValueError('Система вернула пустой датасет. Убедитесь, что вы правильно ввели параметры.')
-        class_col = df.iloc[0]['class_column']
-        kls_df = {
-            class_col: df['class'].tolist()
-        }
-        _classes_df = DataFrame(data=kls_df, index=df['patient_id'].tolist())
-        del df['class']
-        del df['class_column']
-        df = pivot_table(data=df,
-                         index=['patient_id'],
-                         values=['feature_value'],
-                         columns=['feature']
-                         )
-        df.columns = df.columns.get_level_values(1)
-        if normalize == "scale":
-            df = df.apply(scale)
-        elif normalize == "std":
-            df = df.apply(standardize)
-        df = df.join(_classes_df).drop_duplicates()
-        return df, class_col
-    elif load_pattern == 'f':
-        df = PostgresDataLoader.load_data(test=test, features=feature_cols, load_pattern='f')
-        if df is None or len(df) == 0:
-            raise ValueError('Система вернула пустой датасет. Убедитесь, что вы правильно ввели параметры.')
-        df = pivot_table(data=df,
-                         index=['patient_id'],
-                         values=['feature_value'],
-                         columns=['feature']
-                         )
-        df.columns = df.columns.get_level_values(1)
-        if normalize == "scale":
-            df = df.apply(scale)
-        elif normalize == "std":
-            df = df.apply(standardize)
-        return df, None
-    return None
+    test_recs = TestRec.objects.filter(short_name=test).values()
+    if len(test_recs) <= 0:
+        raise ValueError('Система вернула пустой датасет. Убедитесь, что вы правильно ввели параметры.')
+    df = list(map(rec_to_row(class_col=class_col, feature_cols=feature_cols), test_recs))
+    df = DataFrame(data=df)
+    df.set_index(['patient_id'], drop=True, inplace=True)
+    kls_df = None
+    if class_col is not None:
+        class_col = test_recs[0]['text_inds'][class_col]['name']
+        kls_df = df[class_col]
+        del df[class_col]
+    if normalize == "scale":
+        df = df.apply(scale)
+    elif normalize == "std":
+        df = df.apply(standardize)
+    if class_col is not None:
+        df[class_col] = kls_df
+    return df, class_col
